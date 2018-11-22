db4acf7a1a0e0ee045ef86f2ce357331ed172817
Knut Jackowski <doxanthropos@googlemail.com>
2018-11-22 21:50:53 +0100

    fixed the indention

--- Week1/C/palindrome.c
+++ Week1/C/palindrome.c
@@ -4,19 +4,19 @@
 int pal(char a[]);
 
 int main(int argc, char *argv[]) {
-    printf("%s is %s palindrome\n",
-           argv[1], pal(argv[1]) ? "a" : "not a");
-    return 0;
+  printf("%s is %s palindrome\n",
+      argv[1], pal(argv[1]) ? "a" : "not a");
+  return 0;
 }
 
 int pal(char a[]) {
-    if (strlen(a))
-        if (a[0] != a[strlen(a)])
-            return 0;
-        else {
-            a[strlen(a)] = "\0";
-            return pal(a[1]);
-        }
-    else
-        return 1;
+  if (strlen(a))
+    if (a[0] != a[strlen(a)])
+      return 0;
+    else {
+      a[strlen(a)] = "\0";
+      return pal(a[1]);
+    }
+  else
+    return 1;
 }
